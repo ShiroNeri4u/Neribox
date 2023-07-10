90e7fac9563fcb55fe661916a81b98f5428cd425
Shiro_Neri <Shiro_Neri@iCloud.com>
2023-07-10 20:29:20 +0800

    update

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -2,17 +2,3 @@
 MODDIR=${0%/*}
 NERIBOXDIR="/data/adb/Neribox"
 ln -sf $MODDIR/toolkit $MODDIR/system/bin
-
-find $NERIBOXDIR -name '*.log' -size +10M > $NERIBOXDIR/TMP
-while read LOG;do
-    if [ -f $LOG ];then
-        rm $LOG
-    fi
-done<$NERIBOXDIR/TMP
-if [ -f $NERIBOXDIR/TMP ];then
-    rm $NERIBOXDIR/TMP
-fi
-
-if [ -d /data/adb/Neribox/update-cache ];then
-    rm -r /data/adb/Neribox/update-cache
-fi
